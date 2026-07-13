4a71ee79296718e1fcaa9b9990f37ae4abdf742f
usman334 <awanusman595@gmail.com>
2026-07-13 14:39:32 +0500

    Add input and type casting examples

---
+++ DAY1_typecasting.py
@@ -0,0 +1,20 @@
+name = input("Enter Your Name = ")
+age = int(input("Enter Your Age = "))
+height = float(input("Enter Your height = "))
+weight = int(input("Enter Your Weight = "))
+
+print("Student Information")
+print("Name: " , name)
+print("Age: " , age)
+print("Height: " , height)
+print("Weight: " , weight)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(weight))
+
+
+
+Age = int(input("Enter Your Age = "))
+print(f"Next Year Your Age Will Be {Age + 1}")
