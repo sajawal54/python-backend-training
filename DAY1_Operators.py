f9a0f18ed38109f8a97b51d27e29ecdc7edd98bf
usman334 <awanusman595@gmail.com>
2026-07-13 14:59:07 +0500

    Arithmetic Operators Learning

---
+++ DAY1_Operators.py
@@ -0,0 +1,63 @@
+# 10 + 20 the 10 and 20 are operands and + is operator
+# types of operator are 
+# 1. Arithmetic Operators 
+# 2. Comparison Operators
+# 3. Logical Operators 
+# 4. Assignment Operators 
+# 5. Membership Operators 
+# 6. Identity Operators 
+# 7. Bitwise Operators 
+
+
+
+# Arithmetic Operators
+# Addition
+# price = 500
+# delivery_fee = 100
+
+# total = price + delivery_fee
+
+# print(total)
+
+# substraction
+# balance = 10000
+# withdraw = 2500
+
+# remaining = balance - withdraw
+
+# print(remaining)
+
+# multiplication
+# price = 250
+# quantity = 4
+
+# total = price * quantity
+
+# print(total)
+
+# division
+# marks = 450
+# subjects = 5
+
+# average = marks / subjects
+
+# print(average)
+
+# floor division thorw the deciml part from the answer like 9.89 the answer will be 9 and symbol is //
+
+# % this gives the remainder after division
+
+# exponent ** means raise to the power means if 2**3 the answer will be 8
+
+# WHOLE EXAMPLE
+
+num1 = 50
+num2 = 5
+
+print(f"ADDITION = {num1 + num2}")
+print(f"Subtraction = {num1 - num2}")
+print(f"Multiplication = {num1 * num2}")
+print(f"Division = {num1 / num2}")
+print(f"Floor Division = {num1 // num2}")
+print(f"Modulo = {num1 % num2}")
+print(f"Exponent = {num1 ** num2}")
