b94f8eb1b70e51c931d292d9543d4ae85dba7008
usman334 <awanusman595@gmail.com>
2026-07-13 14:18:42 +0500

    Add variables and data types examples

---
+++ DAY1_datatypes.py
@@ -0,0 +1,17 @@
+full_name = "Rana Sajawal Jameel"
+age = 19
+height = 5.8
+is_backened_student = True
+
+
+print("FULL NAME: " , full_name)
+print(type(full_name))
+
+print("AGE: " , age)
+print(type(age))
+
+print("HEIGHT: " , height)
+print(type(height))
+
+print("BACKENED STUDENT: " , is_backened_student)
+print(type(is_backened_student))
---
+++ DAY1_hello.py
@@ -0,0 +1,31 @@
+# Variables
+# variables kisi data ko store kerte hain think it like a box and data inside it like a product 
+name = "Sajawal"
+age = 19
+gender = "Male"
+print(name , age , gender)
+
+
+# Data Types
+# her data ke liye alag se type hoti hai jaise name sajawal ke liye string 20 ke liye integer or ye sab defualt hoti hain python me
+
+# input
+
+# name = input("Enter Name = ") this allow user to enter a name 
+
+
+# variable me jo variable hota hai wo value ko refer kerta hai mtlb name = sajawal iska mtlbhai name refers to sajawal
+
+# = is assignment which tells store sajaal in variable name
+
+
+x = 5
+x = x + 1
+
+# it means grab the valkue ox which was 5 and add 1 in it and display the output
+
+print(x)  
+
+
+# To see the type of data means if it is string , int , floatz
+print(type(name))
