7762fb4e6e972be2ae4a4b05e89f916d65cb7095
usman334 <awanusman595@gmail.com>
2026-07-13 13:05:37 +0500

    Add Day 1 Introduction Program

---
+++ introduction.py
@@ -0,0 +1,15 @@
+name = input("Enter Your Name = ")
+age = input("Enter Your Age = ")
+city = input("Enter Your City = ")
+university = input("Enter Your University = ")
+
+print("***************Student Profile**************")
+
+print("Name: " , name)
+print("Age: " , age)
+print("City: " , city)
+print("University: " , university)
+
+print("********************************************")
+
+
