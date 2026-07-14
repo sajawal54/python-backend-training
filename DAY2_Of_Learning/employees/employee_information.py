2e5956c9e8cab1f69faf7e1a8feee7e2487fbca4
usman334 <awanusman595@gmail.com>
2026-07-14 15:48:54 +0500

    Learning Pakages and moduling

---
+++ DAY2_Of_Learning/employees/employee_information.py
@@ -0,0 +1,2 @@
+def employee_info(name, salary):
+    return f"Employee: {name}, Salary: {salary}"
---
+++ DAY2_Of_Learning/main.py
@@ -0,0 +1,7 @@
+from utilities import math_operations as mo
+from Modules_Practcice import string_operations as so
+from employees import employee_information as ei
+
+print(mo.add(15 , 20))
+print(so.reverse_string("SAJAWAL"))
+print(ei.employee_info("Sajawal Jameel" , 20000))
